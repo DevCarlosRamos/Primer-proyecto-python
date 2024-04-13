d6f082ce882593c85d0b822233c46f1e1ed8faaf
Carlos Ramos <devcarlosramos@gmail.com>
2024-04-13 13:06:42 -0500

    fist commit

---
+++ index.py
@@ -0,0 +1,5 @@
+# Solicitar al usuario que ingrese su nombre
+nombre = input("Por favor, ingresa tu nombre: ")
+
+# Saludar al usuario utilizando su nombre
+print("Hola, " + nombre + "! Bienvenido al mundo de Python.")
